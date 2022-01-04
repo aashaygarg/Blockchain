16e46d01475bf67c8e7eb4f4ff097cfe13ed2af9
Aashay <aashaygarg2000@gmail.com>
2022-01-05 00:51:17 +0530

    class Blockchain init

--- Module 1/blockchain.py
+++ Module 1/blockchain.py
@@ -1,2 +1,14 @@
 # Module 1 - Create a Blockchain
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
 
+#Part 1 - Building a blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
