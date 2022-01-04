3fe5f238f98ccf1dd28fd96d25e1eaf5f955d189
Aashay <aashaygarg2000@gmail.com>
2022-01-05 01:02:47 +0530

    create_block

--- Module 1/blockchain.py
+++ Module 1/blockchain.py
@@ -12,3 +12,12 @@ class Blockchain:
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
         
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain)+1, 
+                 'timestamp': str(datetime.datetime.now()), 
+                 'proof': proof, 
+                 'previous hash': previous_hash}
+        self.chain.append(block)
+        return block
+    
+    
