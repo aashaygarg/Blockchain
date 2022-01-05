1453203c0caef256c3445ebce449aec328d327a0
Aashay <aashaygarg2000@gmail.com>
2022-01-05 22:31:46 +0530

    mining

--- Module 1/blockchain.py
+++ Module 1/blockchain.py
@@ -51,4 +51,45 @@ class Blockchain:
             block_index+=1
         return True
     
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+
 # 2 - Mining blockchain
+
+@app.route('/mine_block', method = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'we mined a block',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
